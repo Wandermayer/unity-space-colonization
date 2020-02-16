8d85e63d22a9dd0e76ae7f98ee6ab0ac6e34a002
Jason Webb <zen.webb@gmail.com>
2020-02-16 13:36:05 -0600

    Adding Inspector UI buttons for 5 presets

--- Assets/Scripts/GrowthManagerEditor.cs
+++ Assets/Scripts/GrowthManagerEditor.cs
@@ -11,13 +11,23 @@ public class GrowthManagerEditor : Editor {
 
     EditorGUILayout.BeginHorizontal();
 
-    if(GUILayout.Button("Grow")) {
-      manager.GrowInEditor();
-    }
+      if(GUILayout.Button("Grow")) {
+        manager.GrowInEditor();
+      }
 
-    if(GUILayout.Button("Reset")) {
-      manager.ResetScene();
-    }
+      if(GUILayout.Button("Reset")) {
+        manager.ResetScene();
+      }
+
+    EditorGUILayout.EndHorizontal();
+
+    EditorGUILayout.BeginHorizontal();
+
+      if(GUILayout.Button("1")) { manager.LoadPreset1(); }
+      if(GUILayout.Button("2")) { manager.LoadPreset2(); }
+      if(GUILayout.Button("3")) { manager.LoadPreset3(); }
+      if(GUILayout.Button("4")) { manager.LoadPreset4(); }
+      if(GUILayout.Button("5")) { manager.LoadPreset5(); }
 
     EditorGUILayout.EndHorizontal();
   }
