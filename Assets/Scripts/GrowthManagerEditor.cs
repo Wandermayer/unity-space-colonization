09d09e57a808462a94f2f8517e33fadfe142c325
Jason Webb <zen.webb@gmail.com>
2020-02-15 20:06:36 -0600

    Disabling custom Inspector to allow default UI to come up

--- Assets/Scripts/GrowthManagerEditor.cs
+++ Assets/Scripts/GrowthManagerEditor.cs
@@ -4,13 +4,13 @@ using UnityEditor;
 
 [CustomEditor(typeof(GrowthManager))]
 public class GrowthManagerEditor : Editor {
-  public override void OnInspectorGUI() {
-    GrowthManager manager = (GrowthManager)target;
+  // public override void OnInspectorGUI() {
+  //   GrowthManager manager = (GrowthManager)target;
 
-    manager.InputRootNode = (Transform)EditorGUILayout.ObjectField("Custom root node", manager.InputRootNode, typeof(Transform), true);
+  //   manager.InputRootNode = (Transform)EditorGUILayout.ObjectField("Custom root node", manager.InputRootNode, typeof(Transform), true);
 
-    manager.AttractionDistance = EditorGUILayout.Slider("Attraction distance", manager.AttractionDistance, 10f, 200f);
-    manager.KillDistance = EditorGUILayout.Slider("Kill distance", manager.KillDistance, 1f, 50f);
-    manager.SegmentLength = EditorGUILayout.Slider("Segment length", manager.SegmentLength, 1f, 100f);
-  }
+  //   manager.AttractionDistance = EditorGUILayout.Slider("Attraction distance", manager.AttractionDistance, 10f, 200f);
+  //   manager.KillDistance = EditorGUILayout.Slider("Kill distance", manager.KillDistance, 1f, 50f);
+  //   manager.SegmentLength = EditorGUILayout.Slider("Segment length", manager.SegmentLength, 1f, 100f);
+  // }
 }
