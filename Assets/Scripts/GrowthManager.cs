bc599633d01dd8d2a6f83dfb3275489a56966e09
Jason Webb <zen.webb@gmail.com>
2020-02-16 13:35:50 -0600

    Abstracting attractor behaviors to state variables, adding more profiler sample points, improving code comments

--- Assets/Scripts/GrowthManager.cs
+++ Assets/Scripts/GrowthManager.cs
@@ -25,6 +25,22 @@ public class GrowthManager : MonoBehaviour {
   private int _numIterations;
   public bool EnableMaxIterations;
 
+  public int AttractorRaycastAttempts;
+  public float AttractorSurfaceOffset;
+
+  private const int ATTRACTORS_ON_MESH = 0;
+  private const int ATTRACTORS_GRID = 1;
+  private const int ATTRACTORS_SPHERE = 2;
+  public int AttractorsType = ATTRACTORS_ON_MESH;
+
+  private const int ATTRACTOR_RAYCAST_OUTWARDS = 0;
+  private const int ATTRACTOR_RAYCAST_INWARDS = 1;
+  public int AttractorRaycastingType = ATTRACTOR_RAYCAST_INWARDS;
+
+  // input gameobject
+  // random point on mesh
+  public int RootVeinType;
+
   private GameObject _bounds;
   private List<GameObject> _obstacles;
   private List<GameObject> _attractorObjects;
@@ -62,6 +78,12 @@ public class GrowthManager : MonoBehaviour {
   private GameObject veinsObject;
   private MeshFilter filter;
 
+
+  /*
+  ========================
+    INITIAL SETUP
+  ========================
+  */
   void Start() {
     AttractionDistance = .3f;
     KillDistance = .05f;
@@ -78,6 +100,9 @@ public class GrowthManager : MonoBehaviour {
     _numIterations = 0;
     EnableMaxIterations = false;
 
+    AttractorRaycastAttempts = 200000;
+    AttractorSurfaceOffset = .01f;
+
     // Retrieve any active bounds meshes
     _bounds = GetAllChildren(GameObject.Find("Bounds"))[0];
     boundsEnabled = _bounds == null ? false : true;
@@ -85,14 +110,21 @@ public class GrowthManager : MonoBehaviour {
     // Retrieve any active obstacles
     _obstacles = GetAllChildren(GameObject.Find("Obstacles"));
 
-    SetupMeshes();
-    CreateAttractors();
-    CreateRootVeins();
-
-    BuildSpatialIndex();
+    SetupMeshes();        // create child GameObjects for veins and TubeRenderer
+    CreateAttractors();   // generate attractors based on mode
+    CreateRootVeins();    // generate root vein nodes
+    BuildSpatialIndex();  // initialize the node spatial index
   }
 
+
+    /*
+    ========================
+      MESHES
+    ========================
+    */
     void SetupMeshes() {
+      Profiler.BeginSample("SetupMeshes");
+
       // Remove all children (veins/tube) that can build up when switching between Editor and Game modes
       while(transform.childCount > 0) {
         DestroyImmediate(transform.GetChild(0).gameObject);
@@ -109,61 +141,92 @@ public class GrowthManager : MonoBehaviour {
       // Set up the tube renderer
       tube = new GameObject("(Temporary) Tubes").AddComponent<TubeRenderer>();
       tube.transform.SetParent(gameObject.transform);
+
+      Profiler.EndSample();
     }
 
+
+    /*
+    ========================
+      ATTRACTORS
+    ========================
+    */
     void CreateAttractors() {
+      Profiler.BeginSample("CreateAttractors");
+
       _attractors.Clear();
 
-      // Points in a sphere
-      // for (int i = 0; i < 1000; i++) {
-      //   _attractors.Add(new Attractor(Random.insideUnitSphere * .75f));
-      // }
+      switch(AttractorsType) {
+        // Create attractors on target mesh(es) using raycasting ------------------------------------
+        case ATTRACTORS_ON_MESH:
+          CreateAttractorsOnMeshSurface();
+          break;
 
-      // Points in a 3D grid
-      // float width = 4f;
-      // float height = 2f;
-      // float depth = 3.5f;
+        // Points in a 3D grid ----------------------------------------------------------------------
+        case ATTRACTORS_GRID:
+          float width = 4f;
+          float height = 2f;
+          float depth = 3.5f;
 
-      // int xResolution = 15;
-      // int yResolution = 10;
-      // int zResolution = 15;
-      // float jitterAmount = .1f;
+          int xResolution = 15;
+          int yResolution = 10;
+          int zResolution = 15;
+          float jitterAmount = .1f;
 
-      // for(int x = 0; x <= xResolution; x++) {
-      //   for(int y = 0; y <= yResolution; y++) {
-      //     for(int z = 0; z <= zResolution; z++) {
-      //       _attractors.Add(
-      //         new Attractor(
-      //           new Vector3(
-      //             x * (width/xResolution) - width/2 + Random.Range(-jitterAmount, jitterAmount),
-      //             y * (height/yResolution) - height/2 + Random.Range(-jitterAmount, jitterAmount),
-      //             z * (depth/zResolution) - depth/2 + Random.Range(-jitterAmount, jitterAmount)
-      //           )
-      //         )
-      //       );
-      //     }
-      //   }
-      // }
+          for(int x = 0; x <= xResolution; x++) {
+            for(int y = 0; y <= yResolution; y++) {
+              for(int z = 0; z <= zResolution; z++) {
+                _attractors.Add(
+                  new Attractor(
+                    new Vector3(
+                      x * (width/xResolution) - width/2 + Random.Range(-jitterAmount, jitterAmount),
+                      y * (height/yResolution) - height/2 + Random.Range(-jitterAmount, jitterAmount),
+                      z * (depth/zResolution) - depth/2 + Random.Range(-jitterAmount, jitterAmount)
+                    )
+                  )
+                );
+              }
+            }
+          }
 
-      // Create Attractors from GameObjects created by AttractorGenerator script
-      CreateAttractorsOnMeshSurface();
+          break;
+
+        // Points in a sphere ----------------------------------------------------------------------
+        case ATTRACTORS_SPHERE:
+          for (int i = 0; i < 1000; i++) {
+            _attractors.Add(new Attractor(Random.insideUnitSphere * .75f));
+          }
+
+          break;
+      }
+
+      Profiler.EndSample();
     }
 
       public void CreateAttractorsOnMeshSurface() {
-        int NumAttempts = 200000;
-        int hitCount = 0;
-        float offset = 0f;
+        Profiler.BeginSample("CreateAttractorsOnMeshSurface");
 
-        for(int i=0; i<NumAttempts; i++) {
+        int hitCount = 0;
+
+        for(int i=0; i<AttractorRaycastAttempts; i++) {
           RaycastHit hitInfo;
 
-          // Outside in raycasting
-          Vector3 startingPoint = Random.onUnitSphere * 5;
-          Vector3 targetPoint = Random.onUnitSphere * .1f;
+          Vector3 startingPoint = Vector3.zero;
+          Vector3 targetPoint = Vector3.zero;
 
-          // Inside out raycasting
-          // Vector3 startingPoint = new Vector3(0f,.5f,0);
-          // Vector3 targetPoint = Random.onUnitSphere * 10f;
+          switch(AttractorRaycastingType) {
+            // Inside-out raycasting
+            case ATTRACTOR_RAYCAST_OUTWARDS:
+              startingPoint = Random.onUnitSphere * 5;
+              targetPoint = Random.onUnitSphere * .1f;
+              break;
+
+            // Outside-in raycasting
+            case ATTRACTOR_RAYCAST_INWARDS:
+              startingPoint = new Vector3(0f,.5f,0);
+              targetPoint = Random.onUnitSphere * 10f;
+              break;
+          }
 
           bool bHit = Physics.Raycast(
             startingPoint,
@@ -177,7 +240,7 @@ public class GrowthManager : MonoBehaviour {
           if(bHit) {
             // How much distance should there be between attractor and hit surface?
             // offset = Random.Range(.015f, .16f);
-            _attractors.Add(new Attractor(hitInfo.point + (hitInfo.normal * offset)));
+            _attractors.Add(new Attractor(hitInfo.point + (hitInfo.normal * AttractorSurfaceOffset)));
 
             // Color rayColor;
             // rayColor = Color.red;
@@ -188,9 +251,19 @@ public class GrowthManager : MonoBehaviour {
         }
 
         // Debug.Log(hitCount + " hits");
+
+        Profiler.EndSample();
       }
 
+
+    /*
+    ========================
+      ROOT VEINS
+    ========================
+    */
     void CreateRootVeins() {
+      Profiler.BeginSample("CreateRootVeins");
+
       _nodes.Clear();
       _rootNodes.Clear();
 
@@ -304,11 +377,22 @@ public class GrowthManager : MonoBehaviour {
       foreach(Node rootNode in _rootNodes) {
         _nodes.Add(rootNode);
       }
+
+      Profiler.EndSample();
     }
 
+
+  /*
+  ========================
+    MAIN PROGRAM LOOP
+  ========================
+  */
   void Update() {
+    // Automatically pause when max iterations reached (if enabled)
     if(EnableMaxIterations && _numIterations > MaxIterations) {
       isPaused = true;
+
+    // Toggle pause on "space"
     } else if(Input.GetKeyUp("space")) {
       isPaused = !isPaused;
     }
@@ -499,6 +583,7 @@ public class GrowthManager : MonoBehaviour {
         // b. Closed venation = only when all vein nodes in relative neighborhood enter KillDistance
       }
 
+      // Remove any attractors that were flagged
       foreach(Attractor attractor in _attractorsToRemove) {
         _attractors.Remove(attractor);
       }
@@ -521,159 +606,177 @@ public class GrowthManager : MonoBehaviour {
       Profiler.EndSample();
     }
 
-    List<CombineInstance> GetBranchMeshes() {
-      Profiler.BeginSample("GetBranchMeshes");
 
-      _branches.Clear();
-      _branchRadii.Clear();
+      /*
+      ========================
+        BRANCH MESHES
+      ========================
+      */
+      List<CombineInstance> GetBranchMeshes() {
+        Profiler.BeginSample("GetBranchMeshes");
 
-      // Recursively populate the _branches array
-      foreach(Node rootNode in _rootNodes) {
-        GetBranch(rootNode);
-      }
+        _branches.Clear();
+        _branchRadii.Clear();
 
-      List<CombineInstance> combineInstances = new List<CombineInstance>();
-      int t = 0;
-
-      // Create continuous tube meshes for each branch
-      foreach(List<Vector3> branch in _branches) {
-        tube.points = new Vector3[branch.Count];
-        tube.radiuses = new float[branch.Count];
-
-        for(int j=0; j<branch.Count; j++) {
-          tube.points[j] = branch[j];
-          tube.radiuses[j] = _branchRadii[t][j];
+        // Recursively populate the _branches array
+        foreach(Node rootNode in _rootNodes) {
+          GetBranch(rootNode);
         }
 
-        tube.ForceUpdate();
+        List<CombineInstance> combineInstances = new List<CombineInstance>();
+        int t = 0;
 
-        CombineInstance cb = new CombineInstance();
-        cb.mesh = Instantiate(tube.mesh);  // Instantiate is expensive AF - needs improvement!
-        cb.transform = tube.transform.localToWorldMatrix;
-        combineInstances.Add(cb);
+        // Create continuous tube meshes for each branch
+        foreach(List<Vector3> branch in _branches) {
+          tube.points = new Vector3[branch.Count];
+          tube.radiuses = new float[branch.Count];
 
-        t++;
-      }
-
-      Profiler.EndSample();
-
-      return combineInstances;
-    }
-
-      private void GetBranch(Node startingNode) {
-        Profiler.BeginSample("GetBranch");
-
-        List<Vector3> thisBranch = new List<Vector3>();
-        List<float> thisRadii = new List<float>();
-        Node currentNode = startingNode;
-
-        if(currentNode.parent != null) {
-          thisBranch.Add(currentNode.parent.position);
-          thisRadii.Add(currentNode.parent.radius);
-        }
-
-        thisBranch.Add(currentNode.position);
-        thisRadii.Add(currentNode.radius);
-
-        while(currentNode != null && currentNode.children.Count > 0) {
-          if(currentNode.children.Count == 1) {
-            thisBranch.Add(currentNode.children[0].position);
-            thisRadii.Add(currentNode.children[0].radius);
-
-            currentNode = currentNode.children[0];
-          } else {
-            foreach(Node childNode in currentNode.children) {
-              GetBranch(childNode);
-            }
-
-            currentNode = null;
+          for(int j=0; j<branch.Count; j++) {
+            tube.points[j] = branch[j];
+            tube.radiuses[j] = _branchRadii[t][j];
           }
-        }
 
-        _branches.Add(thisBranch);
-        _branchRadii.Add(thisRadii);
+          tube.ForceUpdate();
 
-        Profiler.EndSample();
-      }
+          CombineInstance cb = new CombineInstance();
+          cb.mesh = Instantiate(tube.mesh);  // Instantiate is expensive AF - needs improvement!
+          cb.transform = tube.transform.localToWorldMatrix;
+          combineInstances.Add(cb);
 
-    List<CombineInstance> GetPatchMeshes() {
-      Profiler.BeginSample("GetPatchMeshes");
-
-      _patches.Clear();
-      _patchRadii.Clear();
-
-      // Recursively populate the _patches array
-      foreach(Node rootNode in _rootNodes) {
-        GetPatches(rootNode);
-      }
-
-      List<CombineInstance> combineInstances = new List<CombineInstance>();
-      int t = 0;
-
-      // Create continuous tube meshes for each patch
-      foreach(List<Vector3> patch in _patches) {
-        tube.points = new Vector3[patch.Count];
-        tube.radiuses = new float[patch.Count];
-
-        for(int j=0; j<patch.Count; j++) {
-          tube.points[j] = patch[j];
-          tube.radiuses[j] = _patchRadii[t][j];
-        }
-
-        tube.ForceUpdate();
-
-        CombineInstance cb = new CombineInstance();
-        cb.mesh = Instantiate(tube.mesh);  // Instantiate is expensive AF - needs improvement!
-        cb.transform = tube.transform.localToWorldMatrix;
-        combineInstances.Add(cb);
-
-        t++;
-      }
-
-      Profiler.EndSample();
-
-      return combineInstances;
-    }
-
-      private void GetPatches(Node startingNode) {
-        Profiler.BeginSample("GetPatches");
-
-        Node currentNode = startingNode;
-        List<Vector3> thisPatch;
-        List<float> thisRadii;
-
-        while(currentNode != null && currentNode.children.Count > 0) {
-          if(currentNode.children.Count == 1) {
-            currentNode = currentNode.children[0];
-
-          } else if(currentNode.children.Count > 1) {
-            Node previousNode = currentNode.parent == null ? currentNode : currentNode.parent;
-
-            foreach(Node nextNode in currentNode.children) {
-              thisPatch = new List<Vector3>();
-              thisRadii = new List<float>();
-
-              thisPatch.Add(previousNode.position);
-              thisPatch.Add(currentNode.position);
-              thisPatch.Add(nextNode.position);
-
-              thisRadii.Add(previousNode.radius);
-              thisRadii.Add(currentNode.radius);
-              thisRadii.Add(nextNode.radius);
-
-              _patches.Add(thisPatch);
-              _patchRadii.Add(thisRadii);
-
-              GetPatches(nextNode);
-            }
-
-            currentNode = null;
-          }
+          t++;
         }
 
         Profiler.EndSample();
+
+        return combineInstances;
       }
 
+        private void GetBranch(Node startingNode) {
+          Profiler.BeginSample("GetBranch");
+
+          List<Vector3> thisBranch = new List<Vector3>();
+          List<float> thisRadii = new List<float>();
+          Node currentNode = startingNode;
+
+          if(currentNode.parent != null) {
+            thisBranch.Add(currentNode.parent.position);
+            thisRadii.Add(currentNode.parent.radius);
+          }
+
+          thisBranch.Add(currentNode.position);
+          thisRadii.Add(currentNode.radius);
+
+          while(currentNode != null && currentNode.children.Count > 0) {
+            if(currentNode.children.Count == 1) {
+              thisBranch.Add(currentNode.children[0].position);
+              thisRadii.Add(currentNode.children[0].radius);
+
+              currentNode = currentNode.children[0];
+            } else {
+              foreach(Node childNode in currentNode.children) {
+                GetBranch(childNode);
+              }
+
+              currentNode = null;
+            }
+          }
+
+          _branches.Add(thisBranch);
+          _branchRadii.Add(thisRadii);
+
+          Profiler.EndSample();
+        }
+
+
+      /*
+      ========================
+        PATCH MESHES
+      ========================
+      */
+      List<CombineInstance> GetPatchMeshes() {
+        Profiler.BeginSample("GetPatchMeshes");
+
+        _patches.Clear();
+        _patchRadii.Clear();
+
+        // Recursively populate the _patches array
+        foreach(Node rootNode in _rootNodes) {
+          GetPatches(rootNode);
+        }
+
+        List<CombineInstance> combineInstances = new List<CombineInstance>();
+        int t = 0;
+
+        // Create continuous tube meshes for each patch
+        foreach(List<Vector3> patch in _patches) {
+          tube.points = new Vector3[patch.Count];
+          tube.radiuses = new float[patch.Count];
+
+          for(int j=0; j<patch.Count; j++) {
+            tube.points[j] = patch[j];
+            tube.radiuses[j] = _patchRadii[t][j];
+          }
+
+          tube.ForceUpdate();
+
+          CombineInstance cb = new CombineInstance();
+          cb.mesh = Instantiate(tube.mesh);  // Instantiate is expensive AF - needs improvement!
+          cb.transform = tube.transform.localToWorldMatrix;
+          combineInstances.Add(cb);
+
+          t++;
+        }
+
+        Profiler.EndSample();
+
+        return combineInstances;
+      }
+
+        private void GetPatches(Node startingNode) {
+          Profiler.BeginSample("GetPatches");
+
+          Node currentNode = startingNode;
+          List<Vector3> thisPatch;
+          List<float> thisRadii;
+
+          while(currentNode != null && currentNode.children.Count > 0) {
+            if(currentNode.children.Count == 1) {
+              currentNode = currentNode.children[0];
+
+            } else if(currentNode.children.Count > 1) {
+              Node previousNode = currentNode.parent == null ? currentNode : currentNode.parent;
+
+              foreach(Node nextNode in currentNode.children) {
+                thisPatch = new List<Vector3>();
+                thisRadii = new List<float>();
+
+                thisPatch.Add(previousNode.position);
+                thisPatch.Add(currentNode.position);
+                thisPatch.Add(nextNode.position);
+
+                thisRadii.Add(previousNode.radius);
+                thisRadii.Add(currentNode.radius);
+                thisRadii.Add(nextNode.radius);
+
+                _patches.Add(thisPatch);
+                _patchRadii.Add(thisRadii);
+
+                GetPatches(nextNode);
+              }
+
+              currentNode = null;
+            }
+          }
+
+          Profiler.EndSample();
+        }
+
+
+  /*
+  ========================
+    GIZMOS
+  ========================
+  */
   void OnDrawGizmos() {
     Profiler.BeginSample("OnDrawGizmos");
 
@@ -697,6 +800,12 @@ public class GrowthManager : MonoBehaviour {
     Profiler.EndSample();
   }
 
+
+  /*
+  ========================
+    SPATIAL INDEX
+  ========================
+  */
   private void BuildSpatialIndex() {
     Profiler.BeginSample("BuildSpatialIndex");
 
@@ -712,6 +821,12 @@ public class GrowthManager : MonoBehaviour {
     Profiler.EndSample();
   }
 
+
+  /*
+  ========================
+    HELPER FUNCTIONS
+  ========================
+  */
   private Vector3 GetAverageDirection(Node node, List<Attractor> attractors) {
     Profiler.BeginSample("GetAverageDirection");
 
@@ -742,22 +857,37 @@ public class GrowthManager : MonoBehaviour {
     return children;
   }
 
+
+  /*
+  ========================
+    SCENE
+  ========================
+  */
   public void ResetScene() {
     Debug.Log("Reloading scene ...");
 
+    // Restart iteration counter
     _numIterations = 0;
 
+    // Reset nodes
     _nodes.Clear();
     _rootNodes.Clear();
     CreateRootVeins();
     BuildSpatialIndex();
 
+    // Reset and generate new attractors
     CreateAttractors();
 
     SetupMeshes();
     CreateMeshes();
   }
 
+
+  /*
+  ========================
+    PRESETS
+  ========================
+  */
   public void LoadPreset1() {
     Debug.Log("Loading preset 1 ...");
     ResetScene();
