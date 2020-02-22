462ee23d059a72ab7ce90a66e23b441903df12b4
Jason Webb <zen.webb@gmail.com>
2020-02-22 09:08:53 -0600

    Adding independent scene switching script

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+  void Start() {}
+
+  void Update() {
+    if (Input.GetKeyUp("1")) { SceneManager.LoadScene(0); }
+    if (Input.GetKeyUp("2")) { SceneManager.LoadScene(1); }
+    if (Input.GetKeyUp("3")) { SceneManager.LoadScene(2); }
+    if (Input.GetKeyUp("4")) { SceneManager.LoadScene(3); }
+    if (Input.GetKeyUp("5")) { SceneManager.LoadScene(4); }
+  }
+}
